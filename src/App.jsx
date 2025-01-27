7d52f463553ba89da240cb46c3e8d2dfe13ddefc
nithin077 <nithinchinn@gmail.com>
2025-01-28 00:23:44 +0530

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <p className="text-2xl font-bold m-12">Hello React</p>
+      </div>
+    </>
+  )
+}
+
+export default App
